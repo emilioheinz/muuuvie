b6d8ca6759f16a1e19375714c438f5a260e1ce0c
emilioheinz <emiliosheinz@gmail.com>
2020-10-29 21:10:46 -0300

    feat: first commit

---
+++ Muuuvie/Muuuvie/MuuuvieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuuuvieApp.swift
+//  Muuuvie
+//
+//  Created by Emilio Heinzmann on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MuuuvieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
