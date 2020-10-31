ad059920ddcfb66843f4996828fb035462967c12
emilioheinz <emiliosheinz@gmail.com>
2020-10-30 23:07:12 -0300

    feat: splash and icon

---
+++ Muuuvie/Muuuvie/Extensions/Color+Pallete.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Pallete.swift
+//  Muuuvie
+//
+//  Created by Emilio Heinzmann on 30/10/20.
+//
+
+import SwiftUI
+
+extension UIColor {
+
+    static var mainOrange: UIColor { UIColor(named: "mainOrange")! }
+
+}
+
+extension Color {
+
+    static var mainOrange: Color { Color("mainOrange") }
+
+}
