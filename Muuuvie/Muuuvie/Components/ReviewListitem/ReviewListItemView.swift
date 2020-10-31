404da3b167b65312e540f25dcd2575a50a403bed
B4ckes <lucas.e.backes@gmail.com>
2020-10-31 15:50:04 -0300

    feat: started review component

---
+++ Muuuvie/Muuuvie/Components/ReviewListitem/ReviewListItemView.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewListItemView.swift
+//  Muuuvie
+//
+//  Created by Lucas Eduardo Backes on 31/10/20.
+//
+
+import SwiftUI
+
+struct ReviewListItemView: View {
+//    @ObservedObject var image: FetchImage
+    
+    var body: some View {
+        HStack {
+            VStack {
+                
+            }
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 30)
+        .padding(.bottom, 20)
+    }
+}
+
+struct ReviewListItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReviewListItemView()
+            .previewLayout(.fixed(width: 375, height: 165))
+    }
+}
