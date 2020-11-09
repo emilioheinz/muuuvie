a3c5ca48cda2e2802fd705a4c670094725681d61
B4ckes <lucas.e.backes@gmail.com>
2020-11-08 21:51:39 -0300

    feat: added movie videos endpoint

--- Muuuvie/Muuuvie/Api/Enpoint.swift
+++ Muuuvie/Muuuvie/Api/Enpoint.swift
@@ -10,6 +10,7 @@ import Foundation
 enum Endpoint {
     case popularMovies
     case movieDetail(id: Int)
+    case movieVideos(id: Int)
     case moviesInCategory(category: MovieCategoryEnum)
     case artistDetail(id: Int)
     
@@ -19,6 +20,8 @@ enum Endpoint {
             return "movie/popular"
         case .movieDetail(let id):
             return "movie/\(id)"
+        case .movieVideos(let id):
+            return "movie/\(id)/videos"
         case .moviesInCategory(let category):
             return "movie/\(category.rawValue)"
         case .artistDetail(let id):
