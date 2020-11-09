a74ed448527e388a433f14cfae1236ca31308751
B4ckes <lucas.e.backes@gmail.com>
2020-11-08 21:47:15 -0300

    feat: movie video model

---
+++ Muuuvie/Muuuvie/Models/MovieVideo.swift
@@ -0,0 +1,13 @@
+//
+//  MovieVideo.swift
+//  Muuuvie
+//
+//  Created by Lucas Eduardo Backes on 08/11/20.
+//
+
+import Foundation
+
+struct MovieVideo: Decodable {
+    let site: String?
+    let key: String?
+}
